7a9a5ea1d9379fea522d220d9ec98f3dbf171d83
Ayça Doğan <dogan.aycaa@gmail.com>
2026-06-29 21:24:52 +0300

    Musteriler listelendi

---
+++ MVCProje/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCProje.Models.Entity;
+
+namespace MVCProje.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        ShopBaseDbEntities db = new ShopBaseDbEntities();
+        public ActionResult Index()
+        {
+            var values = db.Customers.ToList();
+            return View(values);
+        }
+    }
+}
