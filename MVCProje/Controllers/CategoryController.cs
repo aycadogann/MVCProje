54e4c814b869c4a7732938d1ed26d8476d4b105c
Ayça Doğan <dogan.aycaa@gmail.com>
2026-06-29 21:08:30 +0300

    Kategoriler listelendi

--- MVCProje/Controllers/CategoryController.cs
+++ MVCProje/Controllers/CategoryController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVCProje.Models.Entity;
 
 namespace MVCProje.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+        ShopBaseDbEntities db = new ShopBaseDbEntities();
         public ActionResult Index()
         {
-            return View();
+            var values = db.Categories.ToList();
+            return View(values);
         }
     }
 }
