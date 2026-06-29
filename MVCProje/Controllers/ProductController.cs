14cc05c6a29ecf781c35520325d0a5644b444d39
Ayça Doğan <dogan.aycaa@gmail.com>
2026-06-29 21:19:06 +0300

    Urun Controller eklendi ve listeleme yapildi

---
+++ MVCProje/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCProje.Models.Entity;
+
+namespace MVCProje.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        ShopBaseDbEntities db = new ShopBaseDbEntities();
+        public ActionResult Index()
+        {
+            var values = db.Products.ToList();
+            return View(values);
+        }
+    }
+}
